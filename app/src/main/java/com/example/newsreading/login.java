91a610fd1f4f2c2759f5c4a9f39525d0fbb9401b
SaravanarajuR <SaravanarajuR>
2023-06-14 00:10:20 +0530

    created ui for login signup

--- app/src/main/java/com/example/newsreading/login.java
+++ app/src/main/java/com/example/newsreading/login.java
@@ -5,9 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class login extends AppCompatActivity {
-
+    Private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
